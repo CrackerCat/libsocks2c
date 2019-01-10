a909a1134246f2a31bcb317e69226d43c66f6d7c
Mario Lau <liuminghao233@gmail.com>
2019-01-10 17:42:51 +0800

    ..

---
+++ src/utils/singleton.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <memory>
+template<class T>
+class Singleton
+{
+public:
+#ifdef SINGLETON_RAW_POINTER
+    template<typename... Args>
+	static T* GetInstance(Args&&... args)
+	{
+		static T* instance = new T(std::forward<Args>(args)...);
+		return instance;
+	}
+#else
+    template<typename... Args>
+    static std::shared_ptr<T> GetInstance(Args&&... args)
+    {
+        static std::shared_ptr<T> instance(std::make_shared<T>(std::forward<Args>(args)...));
+        return instance;
+    }
+#endif
+};
