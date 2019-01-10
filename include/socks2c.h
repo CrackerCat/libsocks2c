222a3cb2c11a26793f63c2e30e4e8e2eae918817
Mario Lau <liuminghao233@gmail.com>
2019-01-10 17:41:15 +0800

    ..

--- include/socks2c.h
+++ include/socks2c.h
@@ -1,3 +1,50 @@
 #pragma once
 
-#include "../src/factory/socks2c_factory.h"
+#include <string>
+
+#if defined(BUILD_DLL) && defined(_WIN32)
+#define OS_Dll_API   __declspec( dllexport )
+#else
+#define OS_Dll_API
+#endif
+
+
+namespace boost
+{
+    namespace asio
+    {
+        class io_context;
+    }
+}
+
+class OS_Dll_API LibSocks2c {
+
+public:
+
+    /*
+     * @Thread Safe
+     *
+     * @Return server id, it's the port actually
+     * 		   0 if there is another server running at that port
+     */
+    static int AsyncRunServer(std::string proxyKey, std::string server_ip, uint16_t server_port, uint64_t timeout = 0);
+    static int AsyncRunClient(std::string proxyKey, std::string socks5_ip, uint16_t socks5_port, std::string server_ip, uint16_t server_port, uint64_t timeout = 0);
+
+    /*
+	 * @Thread Safe
+	 *
+	 * Stop call is asynchronous and it only stops TCP/UDP acceptor, sessions will expire later
+	 *
+	 * @Return true if the inner asyncstop is called
+	 * 		   false if server not exist or you call StopServer() before
+	 */
+    static bool StopProxy(int id);
+
+
+    static void RunClientWithExternContext(boost::asio::io_context &io_context, std::string proxyKey, std::string socks5_ip, uint16_t socks5_port, std::string server_ip, uint16_t server_port, uint64_t timeout = 0);
+
+    static void RunServerWithExternContext(boost::asio::io_context &io_context, std::string proxyKey, std::string server_ip, uint16_t server_port, uint64_t timeout = 0);
+
+};
+
+
--- lib/proxymanager.h
+++ lib/proxymanager.h
@@ -2,43 +2,27 @@
 
 #include "../src/utils/Singleton.h"
 #include "../src/protocol/protocol_def.h"
+#include "proxymap.h"
+
 #include <boost/asio.hpp>
 #include <boost/thread.hpp>
 #include <boost/bind.hpp>
 #include <unordered_set>
-#include "proxymap.h"
 #include <mutex>
 
 
 class ProxyManager : public Singleton<ProxyManager> {
 
-
-    const int check_time = 10;
+    const int check_time = 30;
 
 public:
 
-    ProxyManager() : worker(boost::asio::make_work_guard(io)), timer(io)
-    {
+    ProxyManager();
 
-    }
-
-
-
-
-    void TakeManage(int id)
-    {
-        if (!running) asyncRun();
-
-        std::lock_guard<std::mutex> lg(mutex);
-        printf("take manage: %d\n", id);
-        fflush(stdout);
-
-        managed_proxy_set.insert(id);
-
-    }
-
+    void TakeManage(int id);
 
 private:
+
     boost::asio::io_context io;
     boost::asio::executor_work_guard<boost::asio::io_context::executor_type> worker;
     boost::asio::deadline_timer timer;
@@ -49,52 +33,9 @@ private:
 
     std::unordered_set<int> managed_proxy_set;
 
-    void onTimeUp(const boost::system::error_code &ec)
-    {
-        if (ec)
-        {
-            LOG_ERROR("onTimeUp err --> {}", ec.message().c_str())
-            return;
-        }
+    void onTimeUp(const boost::system::error_code &ec);
 
-        LOG_DEBUG("[{}] onTimeUp", (void*)this)
-
-        std::lock_guard<std::mutex> lg(mutex);
-
-        auto proxy_map = ProxyMap<Protocol>::GetInstance();
-        for (auto it = managed_proxy_set.begin(); it != managed_proxy_set.end(); ) {
-
-            printf("get: %d\n",*it);
-            fflush(stdout);
-            if(proxy_map->ClearProxy(*it)) {
-                printf("erase: %d\n",*it);
-                fflush(stdout);
-                it = managed_proxy_set.erase(it);
-            }
-            else {
-                printf("not erase: %d\n",*it);
-                fflush(stdout);
-                it++;
-            }
-
-        }
-
-
-        timer.expires_from_now(boost::posix_time::seconds(check_time));
-        timer.async_wait(boost::bind(&ProxyManager::onTimeUp, this, boost::asio::placeholders::error));
-
-    }
-
-    void asyncRun()
-    {
-
-        timer.expires_from_now(boost::posix_time::seconds(check_time));
-        timer.async_wait(boost::bind(&ProxyManager::onTimeUp, this, boost::asio::placeholders::error));
-
-        boost::thread t(boost::bind(&boost::asio::io_context::run, &io));
-
-        running = true;
-    }
+    void asyncRun();
 };
 
 
