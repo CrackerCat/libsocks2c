39968d1eb30ec630299cdc67015cef0b2ab9c9ff
Mario Lau <liuminghao233@gmail.com>
2019-02-25 19:27:28 +0800

    ..

--- src/utils/macro_def.h
+++ src/utils/macro_def.h
@@ -1,7 +1,7 @@
 #pragma once
 #ifdef _WIN32
-#define likely(x)
-#define unlikely(x)
+#define likely(x) x
+#define unlikely(x) x
 #else
 #define likely(x)       __builtin_expect((x),1)
 #define unlikely(x)     __builtin_expect((x),0)
