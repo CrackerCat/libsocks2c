e192c2cc3dca388fffd10a4f391325804586915f
Mario Lau <liuminghao233@gmail.com>
2019-03-18 23:59:34 +0800

    firewall helper fix

--- src/net/raw/raw_proxy_helper/firewall_helper.cpp
+++ src/net/raw/raw_proxy_helper/firewall_helper.cpp
@@ -6,13 +6,13 @@ void FirewallHelper::BlockRst(std::string dst_ip, std::string dst_port)
 {
     //TODO add ip port filter
     std::string filewall_rule = "block drop proto tcp from " + dst_ip + " to any flags R/R";
-
+    LOG_INFO("Setting Firewall Rule: {}", filewall_rule)
 
 }
 #elif __linux__
 void FirewallHelper::BlockRst(std::string dst_ip, std::string dst_port)
 {
-    std::string filewall_rule = "iptables -A OUTPUT -p tcp --tcp-flags RST RST -s " + dst_ip + " -j DROP";
+    std::string filewall_rule = "iptables -A OUTPUT -p tcp --dport " + dst_port + " --tcp-flags RST RST -s " + dst_ip + " -j DROP";
     LOG_INFO("Setting Firewall Rule: {}", filewall_rule)
     system(filewall_rule.c_str());
 }
