25b1146667c8679a716c0e894db478553ccf6d9e
Mario Lau <liuminghao233@gmail.com>
2019-04-11 21:15:58 +0800

    ..

--- src/protocol/protocol_def.h
+++ src/protocol/protocol_def.h
@@ -1,9 +1,13 @@
 #pragma once
 
 #ifdef PROTOCOL_AES256GCM
-#include "custom/netunnel-aes256gcmwithobf/netunnel_protocol.h"
 #include "custom/aes256gcmwithobf/aes256gcmwithobf.h"
-#define Protocol netunnel_aes256gcmwithobf_Protocol
+    #ifdef BUILD_NETUNNEL_SERVER
+        #include "custom/netunnel-aes256gcmwithobf/netunnel_protocol.h"
+        #define Protocol netunnel_aes256gcmwithobf_Protocol
+    #else
+    #define Protocol aes256gcmwithobf_Protocol
+    #endif
 #elif  PROTOCOL_CHACHA20POLY1305
 #include "custom/chacha20poly1305withobf/chacha20poly1305withobf.h"
 #define Protocol chacha20poly1305withobf_Protocol
