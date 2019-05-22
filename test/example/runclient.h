9ca3fbdcd1d92ef55dfdc9d28d18ee125b1058f3
Mario Lau <liuminghao233@gmail.com>
2019-05-22 23:08:34 +0800

    ..

--- test/example/runclient.h
+++ test/example/runclient.h
@@ -13,19 +13,20 @@ void test()
     LibSocks2c::Config config;
     config.isServer = false;
     config.proxyKey = "12345678";
-    config.server_ip = "112.74.160.183";
-    //config.server_ip = "108.61.181.123";
-    config.server_port = 4567;
-	config.server_uout_port = 4444;
+    config.server_ip = "108.61.181.123";
+    config.server_port = 3000;
+	config.server_uout_port = 3001;
+
     config.socks5_ip = "0.0.0.0";
-    config.socks5_port = 5555;
-    config.local_uout_ip = "";
-	config.local_uout_port = 0;
+    config.socks5_port = 1080;
+
+    config.local_uout_ip = "192.168.1.101";
+
     config.resolve_dns = false;
-    config.udp_over_utcp = false;
+    config.udp_over_utcp = true;
     config.timeout = 0;
 
-    config.logtofile = true;
+    config.logtofile = false;
 
     LibSocks2c::StartProxy(config);
 
