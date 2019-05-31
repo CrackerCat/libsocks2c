11cae80540b2c12244f60d0f5579654cf2dc04dc
mario <liuminghao233@gmail.com>
2019-06-01 05:31:18 +0800

    well c doesn't have bool

--- lib/libsocks2c-client.cpp
+++ lib/libsocks2c-client.cpp
@@ -2,22 +2,24 @@
 #include "libsocks2c.h"
 static std::string s5_ip = "127.0.0.1";
 static unsigned short s5_port = 1080;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-	void socks2c_setsocks5(const char* socks5_ip, unsigned short socks5_port)
+	void socks2c_setsocks5(const char* socks5_ip, const char* socks5_port)
 	{
-		
+		s5_ip = socks5_ip;
+		s5_port = atoi(socks5_port);
 	}
 
 
-	int socks2c_start(const char* key, const char* server_ip, unsigned short server_port, bool local_dns)
+	int socks2c_start(const char* key, const char* server_ip, const char* server_port, int local_dns)
 	{
 		LibSocks2c::Config config;
 		config.proxyKey = key;
 		config.server_ip = server_ip;
-		config.server_port = server_port;
+		config.server_port = atoi(server_port);
 		config.socks5_ip = s5_ip;
 		config.socks5_port = s5_port;
 		config.resolve_dns = local_dns;
@@ -26,18 +28,18 @@ extern "C" {
 		return s5_port;
 	}
 
-	int socks2c_start_raw(const char* key, const char* server_ip, unsigned short server_port, unsigned short server_uout_port, bool local_dns, char* ifname, bool dns_uout)
+	int socks2c_start_raw(const char* key, const char* server_ip, const char* server_port, const char* server_uout_port, const char* ifname, int local_dns, int dns_uout)
 	{
 		LibSocks2c::Config config;
 		config.proxyKey = key;
 		config.server_ip = server_ip;
-		config.server_port = server_port;
+		config.server_port = atoi(server_port);
 		config.socks5_ip = s5_ip;
 		config.socks5_port = s5_port;
 		config.resolve_dns = local_dns;
 
 		config.udp_over_utcp = true;
-		config.server_uout_port = server_uout_port;
+		config.server_uout_port = atoi(server_uout_port);
 		config.local_uout_ifname = ifname;
 		config.dnsuout = dns_uout;
 
--- lib/libsocks2c-client.h
+++ lib/libsocks2c-client.h
@@ -10,10 +10,10 @@
 extern "C" {
 #endif
 
-	OS_Dll_API void socks2c_setsocks5(const char* socks5_ip, unsigned short socks5_port);
+	OS_Dll_API void socks2c_setsocks5(const char* socks5_ip, const char* socks5_port);
 
-	OS_Dll_API int  socks2c_start(const char* key, const char* server_ip, unsigned short server_port, bool local_dns);
-	OS_Dll_API int  socks2c_start_raw(const char* key, const char* server_ip, unsigned short server_port, unsigned short server_uout_port, bool local_dns, char* ifname, bool dns_uout);
+	OS_Dll_API int  socks2c_start(const char* key, const char* server_ip, const char* server_port, int local_dns);
+	OS_Dll_API int  socks2c_start_raw(const char* key, const char* server_ip, const char* server_port, const char* server_uout_port, const char* ifname, int local_dns, int dns_uout);
 
 	OS_Dll_API void socks2c_stop(unsigned short instance_id);
 
